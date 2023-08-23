7299777d2699f17d2737e528c5d24a41f6fb921d
Hariprasath1996 <hasathphy96@gmail.com>
2023-08-23 21:51:25 +0530

    practice js

---
+++ script.js
@@ -0,0 +1,61 @@
+`use strict`
+
+//  Write a program that calculates the sum of two numbers and displays the result.
+
+// Prompt the user for input
+// let num1 = parseFloat(prompt("Enter the first number:"));
+// let num2 = parseFloat(prompt("Enter the second number:"));
+
+// let sum = num1 + num2;
+
+// console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+
+
+//  Write a program that calculates the area of a rectangle. Prompt the user to enter the length and width of the rectangle and display the result.
+
+// area of rectangle = length * width
+
+// let height = prompt(`Enter the value of height `)
+
+// let width = prompt (`Enter the value of width`)
+
+// let areaOfRectangle = height*width
+
+// console.log(`Given area of rectangle is :` +areaOfRectangle+ );
+
+
+//  Write a program that converts a temperature from Celsius to Fahrenheit. Prompt the user to enter the temperature in Celsius and display the equivalent temperature in Fahrenheit.
+
+// let Celsius = prompt(`temperature in celsius`)
+
+// let Fahrenheit =  (Celsius * 9/5) + 32
+
+// console.log(`temperature in celsius : `+Fahrenheit );
+
+
+
+//  Write a program that checks if a given number is even or odd. Prompt the user to enter a number and display whether it is even or odd.
+
+// let number = prompt(` enter a number `)
+
+// number % 2 === 0 ? console.log(`even`) : console.log(`odd`); 
+
+
+//  Write a program that concatenates two strings and displays the result. Prompt the user to enter two strings and display the concatenated string.
+
+// let fName =  prompt(`enter a first name`)
+// let sName = prompt (`enter a second name`)
+// let  concatenates = fName+sName
+// console.log(`concatenates two strings :` + concatenates);
+
+
+//  Write a program that calculates the average of three numbers. Prompt the user to enter three numbers and display the average.
+
+// let num1 = prompt("Enter the first number: ")
+// let num2 = prompt("Enter the second number: ")
+// let num3 = prompt("Enter the third number: ")
+// average = (num1 + num2 + num3) / 3
+// console.log("Average:"+ average)
+
+
